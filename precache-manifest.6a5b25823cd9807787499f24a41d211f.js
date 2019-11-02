afe1d64364d0a5ae26c4ab83ce672dece4c5d072
Dylan Davis <dylandavispc@gmail.com>
2019-11-02 07:27:08 -0400

    Updates

---
+++ precache-manifest.6a5b25823cd9807787499f24a41d211f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c8c110d3bf0a232cd079914822b2a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d624766b964b849eb8a2",
+    "url": "/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "9cad5ffa32b531f85d25",
+    "url": "/static/css/main.626e1fd6.chunk.css"
+  },
+  {
+    "revision": "d624766b964b849eb8a2",
+    "url": "/static/js/2.9282f4a7.chunk.js"
+  },
+  {
+    "revision": "9cad5ffa32b531f85d25",
+    "url": "/static/js/main.ff54cd54.chunk.js"
+  },
+  {
+    "revision": "f188bec4c20a3f20c7ca",
+    "url": "/static/js/runtime-main.f97e4978.js"
+  },
+  {
+    "revision": "915022154258115a3b50b667e3b69054",
+    "url": "/static/media/linkTransparent.91502215.png"
+  },
+  {
+    "revision": "ee8eb1a8769f78285be26dd3db6fef62",
+    "url": "/static/media/zoraGuitar.ee8eb1a8.png"
+  }
+]);
